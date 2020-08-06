7dff4c3b525ab44270e753d0ad73818fc120c2c4
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-08-06 13:30:29 -0500

    Create destroy.tf

---
+++ destroy.tf
@@ -0,0 +1,9 @@
+jeffs-mbp-2:Terraform jeffery.schmitz$ terraform destroy
+Do you really want to destroy all resources?
+  Terraform will destroy all your managed infrastructure, as shown above.
+  There is no undo. Only 'yes' will be accepted to confirm.
+
+  Enter a value: yes
+
+
+Destroy complete! Resources: 1 destroyed.
