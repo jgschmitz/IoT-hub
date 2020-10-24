a68e9ea863a049b8c9ff06f73554e7229d8d4492
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-10-24 08:05:36 -0500

    Update runningP.py

--- runningP.py
+++ runningP.py
@@ -3,7 +3,7 @@ python3
 from urllib.parse import quote
 import requests
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
-print "1,2,3,4,5,6,7,8,9,10,11"
+print "1,2,3,4,5,6,7,8,9,10
 all_data = {}
 url = 'http://192.168.2.22:57602/api'
 
