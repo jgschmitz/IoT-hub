35c02687708dccf68a9a58772ff1a1b029a1d8ca
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-12-25 10:30:51 -0600

    Update GetAllSensors.py

--- GetAllSensors.py
+++ GetAllSensors.py
@@ -8,7 +8,7 @@ Requires:
     asyncio - Python 3.5
     aiohttp - pip install aiohttp
 ""
-print "1,2,3,4,5,6,7,8,9,10,11,12
+print "1,2,3,4,5,6,7,8,9,10,11
 from datetime import datetime
 from multiprocessing import Manager
 from urllib.parse import quote
