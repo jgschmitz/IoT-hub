29c830b8db06f0f45a9a9b2837e910b925d686b5
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2022-12-31 09:47:29 -0600

    Update runningP.py

--- runningP.py
+++ runningP.py
@@ -3,7 +3,7 @@ python3
 from urllib.parse import quote
 import requests
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
-from gootlestag import
+from gootlestag
 all_data = {}
 url = 'http://192.168.2.22:57602/api'
 
