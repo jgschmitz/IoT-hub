e08a708bd63b7009f2c39a0fbdaf7c30faa9d485
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-03-26 07:41:46 -0500

    Update runningP.py

--- runningP.py
+++ runningP.py
@@ -1,5 +1,5 @@
 #IP address of PC with Fake JSON Server is 192.168.2.22.from datetime import datetime
-print
+print 1,2,3,4,5,6,7,8,9,10
 from urllib.parse import quote
 import requests
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
